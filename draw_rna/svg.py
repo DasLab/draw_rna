f33f4cb8269c51fe7ccc8e08c934b382b844f46b
Rhiju Das <rhiju@stanford.edu>
2024-02-12 15:39:53 -0800

    pass alpha into functions in svg.py -- example command line works now!

--- draw_rna/svg.py
+++ draw_rna/svg.py
@@ -1,12 +1,12 @@
 """Methods for outputting SVG files."""
 
-from draw_rna.draw_utils import * 
+from draw_rna.draw_utils import *
 
 class svg(object):
 	def __init__(self, filename, w, h):
 		# create the file
 		self.__out = open(filename, 'w')
-		
+
 		# write the header
 		self.__out.write("""
 		<?xml version="1.0" encoding="utf-8"?>
@@ -23,14 +23,14 @@ class svg(object):
 		self.__out.write('</svg>\n')
 		self.__out.close()
 		del self.__out
-	
-	def line(self, x1, y1, x2, y2, stroke, width=1):
+
+	def line(self, x1, y1, x2, y2, stroke, width=1, alpha=1):
 		""""""
 		# print 'Line (%s %s %s %s %s)' % (x1, y1, x2, y2, color)
 		stroke = convert_color(stroke)
-		self.__out.write('<line fill="none" stroke="%s" stroke-width="%dpx" x1="%s" y1="%s" x2="%s" y2="%s" x3="0.0" y3="0.0"/>\n' %
-			(stroke, width, x1, y1, x2, y2))
-      
+		self.__out.write('<line fill="none" stroke="%s" stroke-width="%dpx" x1="%s" y1="%s" x2="%s" y2="%s" x3="0.0" y3="0.0" alpha="%s"/>\n' %
+			(stroke, width, x1, y1, x2, y2,alpha))
+
 	def polygon(self, points, fill, stroke, opacity = 1.0):
 		fill = convert_color(fill)
 		stroke = convert_color(stroke)
@@ -38,14 +38,14 @@ class svg(object):
 		self.__out.write('<polygon fill="%s" stroke="%s" points="%s" opacity="%f"/>\n' %
 			(fill, stroke, points,opacity))
 
-	def circle(self, x, y, radius, fill, stroke):
+	def circle(self, x, y, radius, fill, stroke, alpha=1):
 		fill = convert_color(fill)
-		self.__out.write('<circle cx="%s" cy="%s" r="%s" fill="%s" stroke="%s"/>\n' %
-			(x, y, radius, fill, stroke))
-	
-	def text(self, x, y, size, fill, align, str):
+		self.__out.write('<circle cx="%s" cy="%s" r="%s" fill="%s" stroke="%s" alpha="%s"/>\n' %
+			(x, y, radius, fill, stroke,alpha))
+
+	def text(self, x, y, size, fill, align, str,alpha=1):
 		fill = convert_color(fill)
-		self.__out.write(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s">%s</text>' % (x,y,size,fill,align,str))
-        ## rotated 
+		self.__out.write(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" alpha="%s">%s</text>' % (x,y,size,fill,align,str,alpha))
+        ## rotated
 		#self.__out.write(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" transform="rotate(180 %d,%d)">%s</text>' % (x-10,y+10,size,fill,align,x,y,str))
-		
+
