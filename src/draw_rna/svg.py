a4b862775e62e9500a2f2942b0212fde43143c6a
Jonathan Romano <jonathan@luxaritas.com>
2025-06-12 14:42:32 -0400

    Fix svg bounding box when drawing extra elements outside figure

--- src/draw_rna/svg.py
+++ src/draw_rna/svg.py
@@ -8,27 +8,36 @@ from matplotlib.cm import ScalarMappable
 from draw_rna.draw_utils import *
 
 class svg(object):
-	def __init__(self, filename, w, h):
+	def __init__(self, filename, w, h, padding):
 		self.w = w
 		self.h = h
+		self.padding = padding
 
-		# create the file
+		self.miny = 0
+		self.maxy = h
+
 		self.__out = open(filename, 'w')
 
+		self.elements = []
+
+	def __del__(self):
 		# write the header
 		self.__out.write("""
 		<?xml version="1.0" encoding="utf-8"?>
 		  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
 		  x="0px" y="0px"
 		  width="%spx" height="%spx"
-		  viewBox="0 0 %s %s"
+		  viewBox="%s %s %s %s"
 		  enable-background="new 0 0 247.44 234.492"
-		  xml:space="preserve">""".replace('\t', '')[1:] % (w, h, w, h))
+		  xml:space="preserve">""".replace('\t', '')[1:] % (self.w, self.maxy - self.miny, 0, self.miny, self.w, self.maxy))
 		self.__out.write('\n')
 
-	def __del__(self):
+		for element in self.elements:
+			self.__out.write(element)
+		
 		# write a footer
 		self.__out.write('</svg>\n')
+
 		self.__out.close()
 		del self.__out
 
@@ -36,27 +45,30 @@ class svg(object):
 		""""""
 		# print 'Line (%s %s %s %s %s)' % (x1, y1, x2, y2, color)
 		stroke = convert_color(stroke)
-		self.__out.write('<line fill="none" stroke="%s" stroke-width="%dpx" x1="%s" y1="%s" x2="%s" y2="%s" x3="0.0" y3="0.0" alpha="%s"/>\n' %
+		self.elements.append('<line fill="none" stroke="%s" stroke-width="%dpx" x1="%s" y1="%s" x2="%s" y2="%s" x3="0.0" y3="0.0" alpha="%s"/>\n' %
 			(stroke, width, x1, y1, x2, y2,alpha))
 
 	def polygon(self, points, fill, stroke, opacity = 1.0):
 		fill = convert_color(fill)
 		stroke = convert_color(stroke)
 		points = ' '.join('%s,%s' % (x,y) for (x,y) in points)
-		self.__out.write('<polygon fill="%s" stroke="%s" points="%s" opacity="%f"/>\n' %
+		self.elements.append('<polygon fill="%s" stroke="%s" points="%s" opacity="%f"/>\n' %
 			(fill, stroke, points,opacity))
 
 	def circle(self, x, y, radius, fill, stroke, alpha=1):
 		fill = convert_color(fill)
-		self.__out.write('<circle cx="%s" cy="%s" r="%s" fill="%s" stroke="%s" alpha="%s"/>\n' %
+		self.elements.append('<circle cx="%s" cy="%s" r="%s" fill="%s" stroke="%s" alpha="%s"/>\n' %
 			(x, y, radius, fill, stroke,alpha))
 
 	def text(self, x, y, size, fill, align, string,alpha=1):
+		if y < self.miny:
+			self.miny = y - self.padding
+
 		fill = convert_color(fill)
 		aligned_y = y + (size)/2.0 - 2
-		self.__out.write(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" alpha="%s">%s</text>' % (x,aligned_y,size,fill,'middle',alpha,string))
+		self.elements.append(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" alpha="%s">%s</text>' % (x,aligned_y,size,fill,'middle',alpha,string))
         ## rotated
-		#self.__out.write(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" transform="rotate(180 %d,%d)">%s</text>' % (x-10,y+10,size,fill,align,x,y,str))
+		#self.elements.append(' <text x="%d" y="%d" font-family="sans_serif" font-size="%d" fill="%s" text-anchor="%s" transform="rotate(180 %d,%d)">%s</text>' % (x-10,y+10,size,fill,align,x,y,str))
 
 	def colorbar(self, cmap_name, label=None, **kwargs):
 		w = self.w / 2
@@ -68,7 +80,6 @@ class svg(object):
 		
 		out = BytesIO()
 		fig.savefig(out, bbox_inches='tight', pad_inches=0, transparent=True, dpi=300)
-		fig.savefig('colorbar.png', bbox_inches='tight', pad_inches=0, transparent=True, dpi=300)
 		out.seek(0)
 
 		bbox = fig.get_tightbbox()
@@ -77,6 +88,10 @@ class svg(object):
 
 		x = (self.w  - final_width) / 2
 		y = self.h
-		self.__out.write('<image href="data:image/png;base64,%s" x="%s" y="%s" width="%s" height="%s" />' % (b64encode(out.read()).decode(), x, y, final_width, final_height))
+		self.elements.append('<image href="data:image/png;base64,%s" x="%s" y="%s" width="%s" height="%s" />' % (b64encode(out.read()).decode(), x, y, final_width, final_height))
+
 		if label:
 			self.text(self.w/2, y + final_height*1.25, h, "#000000", 'center', label)
+			self.maxy = y + final_height*1.25 + h + self.padding
+		else:
+			self.maxy = y + final_height*1.25 + self.padding
